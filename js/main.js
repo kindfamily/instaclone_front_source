1fb22ecfea21fe0eb868568c5dab2bbe2fe01d37
bill <tuntunkimpo@naver.com>
2019-09-22 19:27:52 +0900

    프론트앤드 소스 다운로드 저장소

---
+++ js/main.js
@@ -0,0 +1,27 @@
+
+window.addEventListener('DOMContentLoaded',function(){
+
+    (function(){
+
+        'use strict';
+
+        const tiger = document.querySelector('.kindtiger');
+
+
+        setTimeout(function(){
+            tiger.classList.add('on');
+        },700);
+
+
+
+
+
+
+
+
+    })();
+
+
+
+
+});
